d41ddcdd62edb9c00906bb3c37a21975f6ad9647
Kyle Millar <kyledmillar@gmail.com>
2022-07-25 02:11:18 +0100

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof num1 != "number" || typeof num2 != "number" || num1 < 0 || num2 < 0) return "ERROR";
+    let result = 0;
+    let min = Math.min(num1, num2);
+    let max = Math.max(num1, num2);
+    for (let i = min; i <= max; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
