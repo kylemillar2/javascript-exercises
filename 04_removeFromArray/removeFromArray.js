bb6e835981ad36225ab073e5fec49a357dd8af39
Kyle Millar <kyledmillar@gmail.com>
2022-07-25 02:01:34 +0100

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for (let i = 1; i < arguments.length; i++) {
+        array = array.filter(item => item !== arguments[i]);
+    }
+    return array;
 };
 
 // Do not edit below this line
