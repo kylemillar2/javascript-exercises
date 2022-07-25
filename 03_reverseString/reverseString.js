62f7564d56885264b7b2022db32730536205e4b6
Kyle Millar <kyledmillar@gmail.com>
2022-07-25 01:15:58 +0100

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("");
+    array = array.reverse();
+    string = array.join("");
+    return string;
 };
 
 // Do not edit below this line
