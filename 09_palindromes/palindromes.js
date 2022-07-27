71f9f79a75275a2d80bb194f5a7b53abf2184ace
Kyle Millar <kyledmillar@gmail.com>
2022-07-28 00:31:40 +0100

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    str = str.replace(/[^a-z0-9]/gi, "")
+    let arr = str.split("");
+    let reversed = (arr.reverse()).join("");
+    return reversed.toLowerCase() === str.toLowerCase();
+}; 
 
 // Do not edit below this line
 module.exports = palindromes;
