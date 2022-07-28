ae9b4749496a30f745f51b7e20dfa498c65be8a9
Kyle Millar <kyledmillar@gmail.com>
2022-07-28 18:31:44 +0100

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const oldest = arr.reduce((a, b) => {
+        let aAge = 0;
+        if (a != 0) {
+            a.hasOwnProperty("yearOfDeath") ? aAge = a.yearOfDeath - a.yearOfBirth 
+                                            : aAge = new Date().getFullYear() - a.yearOfBirth;
+        }
+        b.hasOwnProperty("yearOfDeath") ? bAge = b.yearOfDeath - b.yearOfBirth
+                                        : bAge = new Date().getFullYear() - b.yearOfBirth;
+        console.log(aAge, bAge)
+        return (bAge > aAge) ? b : a;
+    }, 0)
+    return oldest;
 };
 
 // Do not edit below this line
