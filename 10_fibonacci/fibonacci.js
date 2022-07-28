10b75659be3d143a1f5ca0ee8d570bf7662c5804
Kyle Millar <kyledmillar@gmail.com>
2022-07-28 17:32:35 +0100

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num < 0) return "OOPS";
+    let arr = [0, 1];
+    for (let i = 1; i < num; i++) {
+        let prev1 = arr[i]
+        let prev2 = arr[i-1]
+        arr.push(prev1 + prev2);
+    }
+    return arr[num];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
